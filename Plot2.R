1b25f4a99dd3da2b0b526a7fe1764a04978e5551
Kyle Scully <scullykns@gmail.com>
2014-07-13 10:27:10 -0700

    each script reads in data and subsets

--- Plot2.R
+++ Plot2.R
@@ -11,9 +11,35 @@
 #  * Recreates the plot in figure/unnamed-chunk-3.png
 #
 
+#@DOWNLOAD FILE
+###############
+
+if (!file.exists("./household_power_consumption.txt")){
+	download.file(
+		"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
+		destfile="./household_power_consumption.zip",
+		method="curl"
+	)
+
+	unzip("./household_power_consumption.zip")
+}
 
 #@READ DATA
 #############
+data <- read.table(
+	"./household_power_consumption.txt",
+	sep=";",
+	header=TRUE,
+	stringsAsFactors=FALSE,
+	na.strings="?"
+	)
+
+#@SUBSET DATA
+#############
+dataset <- as.Date(data$Date,"%d/%m/%Y")
+dataset <- cbind(dataset, data)
+colnames(dataset)[1] <- "Date_obj"
+dataset <- subset(dataset, Date_obj>="2007-02-01" & Date_obj<="2007-02-02")
 
 #@PLOT DATA
 #############
